d49b2c221544fbaf2f89d4e7679dedccd7060e4f
zpskek <wjdghdwns0@gmail.com>
2021-08-28 11:49:29 +0900

    Remove AxiosResponse interface

--- src/services/videoService.ts
+++ src/services/videoService.ts
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-declare module "axios" {
-  export interface AxiosResponse<T = any> extends Promise<T> {}
-}
-
 const api = axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3/",
   params: {
