23fafa25af5f07817f038e30e6c14abfd41bf3a4
zpskek <wjdghdwns0@gmail.com>
2021-08-29 17:58:42 +0900

    Create Header UI

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,14 +3,22 @@ import { Link } from "react-router-dom";
 
 const Header = memo(() => {
   return (
-    <div className="bg-black text-white w-full">
-      <img src="" alt="" />
-      <Link to="/">Youtube</Link>
-      <form action="#">
-        <input type="text" placeholder="Search..." />
-        <button>Search</button>
+    <header className="bg-black text-white w-full flex items-center p-2">
+      <Link to="/" className="flex items-center mr-6">
+        <img src="/images/logo.png" alt="Youtube" className="h-10" />
+        <h1 className="text-xl font-bold ml-2">Youtube</h1>
+      </Link>
+      <form action="#" className="flex items-center flex-grow">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="flex-grow px-2 py-1 outline-none text-black"
+        />
+        <button type="submit" className="bg-gray-400 p-1">
+          <img src="/images/search.png" alt="Search" className="h-6" />
+        </button>
       </form>
-    </div>
+    </header>
   );
 });
 
